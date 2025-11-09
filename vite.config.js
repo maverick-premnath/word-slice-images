951a07c972f587a6f018df2e6cad77e8ddb0a22d
maverick-premnath <maverickpremnath@gmail.com>
2025-11-09 16:18:11 +0530

    git deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/word-slice-images/',
   plugins: [react()],
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
 
